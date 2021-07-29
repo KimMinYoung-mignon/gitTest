1f756750083e5aa5fca8eb3f9341f1afb19a6de0
KimMinYoung <prayforall.326@gmail.com>
2021-07-29 13:38:26 +0900

    20210729 반응형 헤더 완성

--- portfolio/PHP/NationalLibrary - 복사본/js/default.js
+++ portfolio/PHP/NationalLibrary - 복사본/js/default.js
@@ -44,10 +44,46 @@ $(function(){    ////////// language 토글
 
 $(function(){    ////////// 햄버거버튼 내 하단 nav 시작
 
-    $('.m_TopMenu').click(function(){               //클릭시 옵션 드롭다운
+    $('.m_TopMenu_Text_1').click(function(){               //클릭시 옵션 드롭다운
         $(this).toggleClass('active');                     //열림 화살표 변경
-        $('#m_TopMenu-ul').slideToggle('active');
+        $('#m_TopMenu_ul_1').slideToggle('active');
     });
+
+
+    $('.m_TopMenu_Text_2').click(function(){               //클릭시 옵션 드롭다운
+        $(this).toggleClass('active');                     //열림 화살표 변경
+        $('#m_TopMenu_ul_2').slideToggle('active');
+    });
+
+
+    $('.m_TopMenu_Text_3').click(function(){               //클릭시 옵션 드롭다운
+        $(this).toggleClass('active');                     //열림 화살표 변경
+        $('#m_TopMenu_ul_3').slideToggle('active');
+    })
+
+
+    $('.m_TopMenu_Text_4').click(function(){               //클릭시 옵션 드롭다운
+        $(this).toggleClass('active');                     //열림 화살표 변경
+        $('#m_TopMenu_ul_4').slideToggle('active');
+    })
+
+    $('.m_TopMenu_Text_5').click(function(){               //클릭시 옵션 드롭다운
+        $(this).toggleClass('active');                     //열림 화살표 변경
+        $('#m_TopMenu_ul_5').slideToggle('active');
+    })
+
+
+    $('.m_TopMenu_Text_6').click(function(){               //클릭시 옵션 드롭다운
+        $(this).toggleClass('active');                     //열림 화살표 변경
+        $('#m_TopMenu_ul_6').slideToggle('active');
+    })
+
+
+    $('.m_TopMenu_Text_7').click(function(){               //클릭시 옵션 드롭다운
+        $(this).toggleClass('active');                     //열림 화살표 변경
+        $('#m_TopMenu_ul_7').slideToggle('active');
+    })
+
 
 });
 
